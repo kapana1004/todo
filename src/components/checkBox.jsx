5ca1822b664a9850632a9ff4190a5d294f8c26c6
Mac <mac@Macs-MacBook-Pro.local>
2022-10-26 16:47:46 +0400

    added function  active items left count render

--- src/components/checkBox.jsx
+++ src/components/checkBox.jsx
@@ -1,98 +1,84 @@
-import React, { useState } from 'react'
-import path from '../images/path.png'
-import cross from '../images/icon-cross.svg'
+import React, { useState, useEffect } from "react";
+import path from "../images/path.png";
+import cross from "../images/icon-cross.svg";
 
- 
+const CheckBox = ({
+  todoItem,
+  todoList,
+  setTodoList,
+  filteredList,
+  setFilteredList,
+  setItems,
+}) => {
+  // const [completed, setCompleted] = useState(false)
 
+  const handlingClick = (id) => {
+    // setCompleted(!completed)
 
+    const newList = [...filteredList];
+    console.log(newList);
+    const changedArray = newList.map((todo) => {
+      if (todo.id === id) {
+        todo.status = !todo.status;
+      }
 
-const CheckBox = ({todoItem, todoList, setTodoList, filteredList, setFilteredList} ) => {
+      return todo;
+    });
 
-    // const [completed, setCompleted] = useState(false)
-  
+    setFilteredList(changedArray);
+  };
 
-    const handlingClick = (id) => {
+  useEffect(() => {
+    const actItemsLength = [...filteredList].filter(
+      (todo) => !todo.status
+    ).length;
+    setItems(actItemsLength);
+    console.log(filteredList);
+  }, [filteredList]);
 
-      // setCompleted(!completed)
-
-      const newList = [...todoList]
-
-      const changedArray = newList.map((todo) => {
-       
-        if (todo.id === id){
-
-          todo.status = !todo.status
-
-          
-        }
-
-        return todo
-
-      })
-
-      setTodoList(changedArray)
-
-       
-    }
-
-
-    const handleDelete = (todoItemId) => {
-
-      const restoredList = filteredList.filter((todoItem) => {
-
-          return todoItemId !== todoItem.id;
-          setFilteredList(restoredList);
-          setTodoList(restoredList);
-      });
-   
-      const newList = filteredList.filter((todoItem) => {
-
-        return todoItemId !== todoItem.id;
-      });
-
-      setTodoList(newList);
+  const handleDelete = (todoItemId) => {
+    const restoredList = filteredList.filter((todoItem) => {
+      return todoItemId !== todoItem.id;
       setFilteredList(restoredList);
-  }
+      setTodoList(restoredList);
+    });
 
+    const newList = filteredList.filter((todoItem) => {
+      return todoItemId !== todoItem.id;
+    });
 
-
-    
-
-   
-
-    
-
-
-
-    
-
+    setTodoList(newList);
+    setFilteredList(restoredList);
+  };
 
   return (
-        <li className={` ${todoItem.status && "line-through"  } relative  pl-[24px] border-y-[1px] h-[52px] text-xs pt-[12px] text-slate-700 w-[100%] flex flex-row rounded-[5px]`} > 
-         
-          <div className={` border border-gray-600 cursor-pointer mr-[12px] w-[20px] h-[20px] ${todoItem.status ? 'bg-gradient-to-br from-blue-500 to-pink-500' : 'bg-white'}  rounded-[50px] flex justify-center items-center`} onClick={ () => handlingClick(todoItem.id)}> 
-          <img src={path} alt='path' />
-            </div> 
-                {todoItem.name} 
+    <li
+      className={` ${
+        todoItem.status && "line-through"
+      } relative  pl-[24px] border-y-[1px] h-[52px] text-xs pt-[12px] text-slate-700 w-[100%] flex flex-row rounded-[5px]`}
+    >
+      <div
+        className={` border border-gray-600 cursor-pointer mr-[12px] w-[20px] h-[20px] ${
+          todoItem.status
+            ? "bg-gradient-to-br from-blue-500 to-pink-500"
+            : "bg-white"
+        }  rounded-[50px] flex justify-center items-center`}
+        onClick={() => handlingClick(todoItem.id)}
+      >
+        <img src={path} alt="path" />
+      </div>
+      {todoItem.name}
 
-                
-            <img  className=' cursor-pointer w-[12px] h-[12px] absolute top-[28%] right-[5%]' src={cross} alt='cross' onClick={() => handleDelete(todoItem.id)} />
-         </li>
-  )}
+      <img
+        className=" cursor-pointer w-[12px] h-[12px] absolute top-[28%] right-[5%]"
+        src={cross}
+        alt="cross"
+        onClick={() => handleDelete(todoItem.id)}
+      />
+    </li>
+  );
+};
 
- 
-
-
-
-
-
-
- 
-
-
-
-
-export default CheckBox
-
+export default CheckBox;
 
 //don`t work checkbox on new addded task
--- src/components/todo-list.jsx
+++ src/components/todo-list.jsx
@@ -1,210 +1,194 @@
+import { buildQueries } from "@testing-library/react";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import Moonimg from "../images/icon-moon.svg";
+import path from "../images/path.png";
+import CheckBox from "./checkBox";
 
-import { buildQueries } from '@testing-library/react';
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import Moonimg from '../images/icon-moon.svg';
-import path from '../images/path.png'
-import CheckBox from './checkBox';
-
-
-
-
-
-const todoItems = 
-
-[
+const todoItems = [
   {
-    name: 'Complete online JavaScript course', 
+    name: "Complete online JavaScript course",
     id: 1,
     status: false,
-  
   },
- { 
-  name:'Jog around the park 3x',
- id: 2,
- status: false, 
-},
-{
-name:'10 minutes meditation',
-id:3,
-status: false,
+  {
+    name: "Jog around the park 3x",
+    id: 2,
+    status: false,
+  },
+  {
+    name: "10 minutes meditation",
+    id: 3,
+    status: false,
+  },
+  {
+    name: "Read for 1 hour",
+    id: 4,
+    status: false,
+  },
 
-},
-{
-name: 'Read for 1 hour',
-id:4,
-status: false,
+  {
+    name: "Pick up groceries",
+    id: 5,
+    status: false,
+  },
 
-},
-
-{
-name: 'Pick up groceries',
-id:5,
-status: false,
-
-},
-
-{
-  name: 'Complete Todo App on Frontend Mentor',
-  id:6,
-  status: false,
-},
-
-
-
-
-
-]
+  {
+    name: "Complete Todo App on Frontend Mentor",
+    id: 6,
+    status: false,
+  },
+];
 
 const Todo = (props) => {
-
-  const [todoList, setTodoList] = useState(todoItems); 
+  const [todoList, setTodoList] = useState(todoItems);
 
   const [filteredList, setFilteredList] = useState(todoItems);
 
-  const [category, setCategory] = useState('all');
+  const [category, setCategory] = useState("all");
 
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
+
+  const [newId, setNewId] = useState(7);
+
+  const [items, setItems] = useState(6);
+
+  // const countItem = () => {
+  //   todoList.map((todo) => {
+  //     if (todo.status === false) {
+  //       setItems(items + 1);
+  //     }
+  //   });
+  // };
 
   const changeHandling = (event) => {
+    if (event.key === "Enter") {
+      // const lastID = todoList[todoList.length - 1]?.id || 0;
+      const newTask = {
+        name: event.target.value,
+        id: newId,
+        status: false,
+      };
+      setNewId(newId + 1);
+      const newTodoItems = [...filteredList, newTask];
 
+      setFilteredList(newTodoItems);
 
-    
-   if (event.key === "Enter" ) {
-    
-    const lastID = todoList[todoList.length-1]?.id || 0
-    const newTask = 
-    {name: event.target.value,
-     id: lastID+1, 
-     status: false}
-
-     const newTodoItems = [...filteredList, newTask]
-     
-     setFilteredList(newTodoItems);
-
-     setInputValue('')
-
-   }
-
-    
-  }
-  
-
-    const handlingActive = () => {
-
-
-      const copiedTodoList = [...todoList]
-
-      const activeArray = copiedTodoList.filter((todo) => {
-
-        return todo.status === false
-      })
-
-      setCategory('active')
-
-      setFilteredList(activeArray)
-      
+      setInputValue("");
     }
+  };
 
-    const handleFilter = () => {
+  const handlingActive = () => {
+    const copiedTodoList = [...todoList];
 
-      setFilteredList(todoList)
+    const activeArray = copiedTodoList.filter((todo) => {
+      return todo.status === false;
+    });
 
-      setCategory('all')
-    }
+    setCategory("active");
 
-    const handleComplete = () => {
+    setFilteredList(activeArray);
+  };
 
+  const handleFilter = () => {
+    setFilteredList(todoList);
 
-      const copiedTodoList = [...todoList]
+    setCategory("all");
+  };
 
-      const activeArray = copiedTodoList.filter((todo) => {
+  const handleComplete = () => {
+    const copiedTodoList = [...todoList];
 
-        return todo.status === true
-      })
+    const activeArray = copiedTodoList.filter((todo) => {
+      return todo.status === true;
+    });
 
-      setFilteredList(activeArray)
-      setCategory('completed')
-    }
+    setFilteredList(activeArray);
+    setCategory("completed");
+  };
 
-    
-
-  
-
- 
-
-  return(
+  return (
     <>
+      <div className=" justify-center items-center w-[375px]">
+        <div className=" items-center w-[350px]">
+          <div className="flex justify-between flex-row pt-[50px] items-center">
+            <span className=" text-3xl ml-[24px]  text-white "> T O D O </span>
 
-   <div className=' justify-center items-center w-[375px]'>
+            <img className=" w-[20px] h-[20px] " src={Moonimg} alt="moon" />
+          </div>
 
-   <div className=' items-center w-[350px]'>
+          <div className=" mt-[20px] flex flex-row ml-[1.5rem] w-[327px] bg-white rounded-[5px]">
+            <div className=" border border-gray-600 ml-[20px] mt-[14px] w-[20px] h-[20px] bg-white rounded-[50px] flex justify-center items-center">
+              {" "}
+              <img src={path} />{" "}
+            </div>
 
-    <div className='flex justify-between flex-row pt-[50px] items-center'>
-    <span className=' text-3xl ml-[24px]  text-white '> T O D O </span>
+            <input
+              type="text"
+              value={inputValue}
+              onKeyDown={changeHandling}
+              onChange={(event) => setInputValue(event.target.value)}
+              className=" outline-none w-[270px] ml-[20px] resize-none  h-[48px]   placeholder:text-xs text-[13px] rounded-[5px]"
+              placeholder="Create a new todo…"
+            ></input>
+          </div>
+        </div>
+        <div>
+          <ul className=" w-[327px] ml-[24px] bg-white mt-[20px]  rounded-[5px]">
+            {filteredList.map((todoItem, index) => {
+              return (
+                <CheckBox
+                  setFilteredList={setFilteredList}
+                  filteredList={filteredList}
+                  todoItem={todoItem}
+                  todoList={todoList}
+                  setTodoList={setTodoList}
+                  setItems={setItems}
+                  key={index}
+                />
+              );
+            })}
 
-    <img className=' w-[20px] h-[20px] ' src={Moonimg} alt="moon" />
+            <div className=" rounded-[5px] w-[327px] h-[50px] flex justify-between p-[20px] items-center">
+              <span className=" cursor-pointer text-xs text-gray-400">
+                {items} items left
+              </span>
+              <span className=" cursor-pointer text-xs text-gray-400">
+                Clear Completed
+              </span>
+            </div>
+          </ul>
 
-      </div> 
-
-      <div className=' mt-[20px] flex flex-row ml-[1.5rem] w-[327px] bg-white rounded-[5px]'> 
-      
-      <div className=' ml-[20px] mt-[14px] w-[20px] h-[20px] bg-gradient-to-br from-blue-500 to-pink-500 rounded-[50px] flex justify-center items-center'> <img src={path} /> </div>
-      
-    <input type='text'  value={inputValue} onKeyDown={changeHandling}  onChange={ (event) => setInputValue(event.target.value)} className=' w-[270px] ml-[20px] resize-none  h-[48px]   placeholder:text-xs text-[13px] rounded-[5px]' placeholder='Create a new todo…' >
-      </input>
-    </div>
-   
-   </div>
-    <div> 
-
-    
- 
-
-    
-
-    <ul className=' w-[327px] ml-[24px] bg-white mt-[20px]  rounded-[5px]'>
-       {filteredList.map((todoItem, index) => {
-
-        return (
-
-
-       <CheckBox setFilteredList = {setFilteredList}  filteredList = {filteredList} todoItem={todoItem}  todoList = {todoList} setTodoList={setTodoList} key={index} />
-        )
-       })}
-     
-       <div className=' rounded-[5px] w-[327px] h-[50px] flex justify-between p-[20px] items-center' > 
-          <span className=' cursor-pointer text-xs text-gray-400'> 5 items left </span>  
-          <span className=' cursor-pointer text-xs text-gray-400'>Clear Completed</span>
-       </div>
-
-    
-       
-   
-       
-   
-
-    </ul>
-
-    <div className=' rounded-[5px] w-[327px] h-[48px]  flex justify-around items-center mt-[20px] ml-[25px] bg-white '> 
-          <span className= {`text-[14px] font-sans cursor-pointer ${category==='all'? 'text-blue-600': 'text-gray-700'}`}  onClick={handleFilter}>All</span> 
-          <span className= {`text-[14px] font-sans cursor-pointer ${category==='active'? 'text-blue-600': 'text-gray-700'}`} onClick={handlingActive}>Active</span>  
-          <span className= {`text-[14px] font-sans cursor-pointer ${category==='completed'? 'text-blue-600': 'text-gray-700'}`} onClick={handleComplete} >Completed</span>
-     </div>
-
-    </div>
-
-    
-    </div>
-
-   
+          <div className=" rounded-[5px] w-[327px] h-[48px]  flex justify-around items-center mt-[20px] ml-[25px] bg-white ">
+            <span
+              className={`text-[14px] font-sans cursor-pointer ${
+                category === "all" ? "text-blue-600" : "text-gray-700"
+              }`}
+              onClick={handleFilter}
+            >
+              All
+            </span>
+            <span
+              className={`text-[14px] font-sans cursor-pointer ${
+                category === "active" ? "text-blue-600" : "text-gray-700"
+              }`}
+              onClick={handlingActive}
+            >
+              Active
+            </span>
+            <span
+              className={`text-[14px] font-sans cursor-pointer ${
+                category === "completed" ? "text-blue-600" : "text-gray-700"
+              }`}
+              onClick={handleComplete}
+            >
+              Completed
+            </span>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Todo
-
-
-
-// footers layout styles doesn`t work out of ul (margin top??)
-// once added todo is replacing by another pushed todo
+export default Todo;
